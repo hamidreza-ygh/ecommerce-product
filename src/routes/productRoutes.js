f74bad74fa8f81fdba49abddc1c1bf467f9c2f95
Hamidreza-ygh <hr.yaghoubi@gmail.com>
2024-06-16 19:00:13 +0200

    Comment local auth middleware

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,9 +5,12 @@ const isAuthenticated = require("../utils/isAuthenticated");
 const router = express.Router();
 const productController = new ProductController();
 
-router.post("/", isAuthenticated, productController.createProduct);
-router.post("/buy", isAuthenticated, productController.createOrder);
-router.get("/", isAuthenticated, productController.getProducts);
+// router.post("/", isAuthenticated, productController.createProduct);
+// router.post("/buy", isAuthenticated, productController.createOrder);
+// router.get("/", isAuthenticated, productController.getProducts);
+router.post("/", productController.createProduct);
+router.post("/buy", productController.createOrder);
+router.get("/", productController.getProducts);
 
 
 module.exports = router;
